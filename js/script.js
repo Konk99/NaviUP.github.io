7c7949c25a2938f2e41a20ad05dbdf33e1b4f71d
Kkonk <konk99@o2.pl>
2022-11-25 00:06:08 +0100

    Fix small image slash bug part9

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.body.style.backgroundImage = document.querySelector('.backimg').getAttr
 
 darkMode.addEventListener('click', (event) => {
     event.preventDefault();
-    bacgroundStatus = getComputedStyle(document.body).backgroundImage.replace('url("http://localhost:3000/images/','').replace('")','');
+    bacgroundStatus = getComputedStyle(document.body).backgroundImage.replace('url("https://konk99.github.io/NaviUP.github.io/images/','').replace('")','');
     if(bacgroundStatus == 'y-so-serious-white.png'){
         document.body.style.backgroundImage = "url('/images/y-so-serious.png')";
     }else if(bacgroundStatus == "y-so-serious.png"){
