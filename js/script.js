353f9bcdb563d5f09f4643d07d37f7bb364c4cc0
Kkonk <konk99@o2.pl>
2022-11-24 22:21:54 +0100

    Fix small image slash bug

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ darkMode.addEventListener('click', (event) => {
     event.preventDefault();
     bacgroundStatus = getComputedStyle(document.body).backgroundImage.replace('url("http://localhost:3000/images/','').replace('")','');
     if(bacgroundStatus == 'y-so-serious-white.png'){
-        document.body.style.backgroundImage = "url('/images/y-so-serious.png')";
+        document.body.style.backgroundImage = "url('images/y-so-serious.png')";
     }else if(bacgroundStatus == "y-so-serious.png"){
-        document.body.style.backgroundImage = "url('/images/y-so-serious-white.png')";
+        document.body.style.backgroundImage = "url('images/y-so-serious-white.png')";
     }
 });
