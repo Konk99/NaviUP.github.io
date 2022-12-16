1b478da5af06c340181e365f02c44753203c6618
Kkonk <konk99@o2.pl>
2022-12-16 16:31:26 +0100

    Fix some misspelling. Add working three.js model

--- js/three.js
+++ js/three.js
@@ -1,26 +1,62 @@
 import * as THREE from '../vendor/three/build/three.module.js';
+import {OrbitControls} from '../vendor/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../vendor/three/examples/jsm/loaders/GLTFLoader.js';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xddddd);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth / 2, window.innerHeight / 2 );
-document.body.appendChild( renderer.domElement );
+const camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight,1,5000);
+camera.position.y = 45/180*Math.Pi;
+camera.position.x = 800;
+camera.position.y = 100;
+camera.position.z = 1000;
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const loader = new GLTFLoader();
+loader.load('../vendor/models/Pomieszczenia.gltf',function(gltf){
+    const model = gltf.scene;
+    model.scale.set(0.1,0.1,0.1);
+    scene.add(gltf.scene);
+    console.log(gltf);
+    animate();
+    },
+    function (xhr) {
+        console.log((xhr.loaded/xhr.total * 100) + '% loaded');
+    });
 
-camera.position.z = 5;
 
-var animate = function () {
-	requestAnimationFrame( animate );
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', renderer);
 
-	renderer.render( scene, camera );
-};
+const hilight = new THREE.AmbientLight(0x404040,100);
+scene.add(hilight);
 
-animate();
+const directionalLight = new THREE.DirectionalLight(0xffffff,100);
+directionalLight.position.set(0,1,0);
+directionalLight.castShadow = true;
+scene.add(directionalLight)
+
+const light = new THREE.PointLight(0xc4c4c4,10);
+light.position.set(0,300,500);
+scene.add(light);
+
+const light2 = new THREE.PointLight(0xc4c4c4,10);
+light2.position.set(500,100,0);
+scene.add(light2);
+
+const light3 = new THREE.PointLight(0xc4c4c4,10);
+light3.position.set(0,100,-500);
+scene.add(light3);
+
+const light4 = new THREE.PointLight(0xc4c4c4,10);
+light4.position.set(-500,300,0);
+scene.add(light4);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
